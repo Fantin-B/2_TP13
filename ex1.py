83b908cbd4b2869ec72d6b1eddff67aa168d903f
Fantin <fantin.brisard@isen-ouest.yncrea.fr>
2020-04-27 23:23:05 +0200

    ex2

--- ex1.py
+++ ex1.py
@@ -1,11 +1,14 @@
 class Node():
-    def __init__(self, val, right, left):
+    def __init__(self, val, left, right):
         self.__val = val
         self.__right = right
         self.__left = left
 
     def getVal(self):
-        return self.__val
+        if self.__val is None:
+            return None
+        else :
+            return self.__val
 
     def getRight(self):
         return self.__right
@@ -14,16 +17,15 @@ class Node():
         return self.__left
 
     def setRight(self,noeud):
-        self.__right.append(noeud)
+        self.__right = noeud
 
     def setLeft(self,noeud):
-        self.__left.append(noeud)
+        self.__left = noeud
 
 
 #test programme
 
-#noeud1 = Node(12,17,5)
-#noeud2 = Node(5,6,4)
-#noeud3 = Node(4,None,3)
-#noeud4 = Node(17,19,None)
-#noeud5 = Node(19,21,18)
+#noeud = Node(None,None,None)
+#print(noeud.getVal())
+#print(noeud.getLeft())
+#print(noeud.getRight())
--- ex2.py
+++ ex2.py
@@ -7,15 +7,99 @@ class BinaryTree():
     def getRoot(self):
         return self.__root
 
+    def isRoot(self, node):
+        if node == self.__root:
+            return True
+        else:
+            return False
+
+
+    def height(self,node):
+
+        if node == None or (node.getLeft()==None and node.getRight()==None) :
+            return 1
+
+        hauteurNoeuddroite = 1+ self.height(node.getRight())
+        hauteurNoeudgauche = 1+ self.height(node.getLeft())
+        hauteurNoeud = max(hauteurNoeuddroite,hauteurNoeudgauche)
+
+        return hauteurNoeud
+
+
+
+    def size(self,node):
+
+        if node == None:
+            return -1
+        if node.getLeft()==None and node.getRight()==None :
+            return 0
+
+        tailleNoeuddroite = 1+ self.size(node.getRight())
+        tailleNoeudgauche = 1+ self.size(node.getLeft())
+        tailleNoeud = tailleNoeuddroite + tailleNoeudgauche
+        print("taille noeud",node.getVal(),":",tailleNoeud)
+        return tailleNoeud
+
+
+
+
+
+
+
+
 
 #test programme
-root = BinaryTree(12)
-noeud1 = Node(root.getRoot(),17,5)
-noeud2 = Node(noeud1.getRight(),6,4)
-noeud3 = Node(noeud2.getRight(),None,None)
-noeud4 = Node(noeud2.getLeft(),None,4)
-noeud5 = Node(noeud4.getLeft(),None,3)
-noeud6 = Node(noeud1.getLeft(),19,None)
-noeud7 = Node(noeud6.getLeft(),21,18)
-noeud8 = Node(noeud7.getRight(),None,None)
-noeud9 = Node(noeud7.getLeft(),None,None)
+root = Node(12,None,None)
+arbre = BinaryTree(root)
+
+noeud5 = Node(5,None,None)
+root.setLeft(noeud5)
+
+noeud6 = Node(6,None,None)
+noeud5.setRight(noeud6)
+
+noeud4 = Node(4,None,None)
+noeud5.setLeft(noeud4)
+
+noeud3 = Node(3,None,None)
+noeud4.setLeft(noeud3)
+
+noeud17 = Node(17,None,None)
+root.setRight(noeud17)
+
+noeud19 = Node(19,None,None)
+noeud17.setRight(noeud19)
+
+noeud21 = Node(21,None,None)
+noeud19.setRight(noeud21)
+
+noeud18 = Node(18,None,None)
+noeud19.setLeft(noeud18)
+
+#print(arbre.height(noeud5))
+print(arbre.size(root))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def affichageNoeud(noeud):
+    print("Valeur :",noeud.getVal())
+    if noeud.getRight() != None:
+        print("Noeud droit :",noeud.getRight().getVal())
+    else :
+        print("Noeud droit :",None)
+    if noeud.getLeft() != None:
+        print("Noeud gauche :",noeud.getLeft().getVal())
+    else :
+        print("Noeud gauche :",None)
+#affichageNoeud(root)
